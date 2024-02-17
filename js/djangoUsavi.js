7bfc45d1c1c461ce70be316b47e5f114120f301e
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2024-02-17 10:24:34 +0000

    added django website display page

---
+++ js/djangoUsavi.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("click", function(event) {
+    if (event.target.classList.contains("btn")) {
+        if (event.target.innerHTML === "Show More..") {
+            event.target.innerHTML = "Show Less..";
+        } else {
+            event.target.innerHTML = "Show More..";
+        }
+    }
+});
